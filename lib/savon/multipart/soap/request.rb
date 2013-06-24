27801e72213a310ffdcdd6f701eb5e31d5a583b7
Rob Anderson <rob.anderson@paymentcardsolutions.co.uk>
2013-06-24 14:07:44 +0100

    remove mail gem content encoding

--- lib/savon/multipart/soap/request.rb
+++ lib/savon/multipart/soap/request.rb
@@ -11,7 +11,10 @@ module Savon
           # takes relevant http headers from the "Mail" message and makes
           # them Net::HTTP compatible
           request_message.header.fields.each do |field|
-            http.headers[field.name] = field.to_s
+            # the mail gem clobbers the text encoding and sets its own (utf-8 in our case)
+            # remove the mail text encoding here
+            # http.headers[field.name] = field.to_s
+            http.headers[field.name] = field.to_s.split(';').delete_if {|s| s =~ /charset/}.join(';')
           end
           #request.headers["Content-Type"] << %|; start="<savon_soap_xml_part>"|
           request_message.body.set_sort_order soap.parts_sort_order if soap.parts_sort_order && soap.parts_sort_order.any?
